5f5f7e953eff57f1b7d33d02251efc98d350fb3e
Sandile <sandilemini1@gmail.com>
2023-11-02 22:16:23 +0200

    first commit

---
+++ MyFirstMauiApp/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace MyFirstMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count += 10;
+
+            if (count == 1)
+                CounterBtn.Text = $"I was clicked {count} time";
+            else
+                CounterBtn.Text = $"I was licked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
